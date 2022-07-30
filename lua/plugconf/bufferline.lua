4f9dd63f02b66d78a126defabf89f26bb41614d3
ccg <aimerchenyuan@163.com>
2022-07-30 11:40:00 +0800

    add bufferlinefill link to Normal guibg

--- lua/plugconf/bufferline.lua
+++ lua/plugconf/bufferline.lua
@@ -16,3 +16,8 @@ options = {
   }
 }
 -- vim.cmd([[ highlight BufferLineFill guibg=#282828 ]])
+-- the bulow comman will link the BufferlineFill guibg to Normal guibg.
+-- cause after update plugin in mac, the catppuccin can not change BufferlineFill color.
+-- vim.cmd([[
+-- exec 'hi BufferlineFill guibg=' . synIDattr(synIDtrans(hlID('Normal')), 'bg', 'gui')
+-- ]])
