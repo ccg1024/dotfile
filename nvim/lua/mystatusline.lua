ecde1485b8c16451055338692328f2cc6c45c2da
ccg <aimerchenyuan@163.com>
2022-09-22 15:15:50 +0800

    a big modify for statusline

--- nvim/lua/mystatusline.lua
+++ nvim/lua/mystatusline.lua
@@ -80,6 +80,17 @@ local function LspErrors()
   return sl
 end
 
+local function MyLspInfo()
+  local sl = ''
+  if not vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
+    local lsp_hints = vim.diagnostic.get(0, { severity=vim.diagnostic.severity.HINT })
+    local lsp_warns = vim.diagnostic.get(0, { severity=vim.diagnostic.severity.WARN })
+    local lsp_error = vim.diagnostic.get(0, { severity=vim.diagnostic.severity.ERROR })
+    sl = sl .. "%#MyLspError#" .. "  " .. #lsp_error .. "%#MyLspHint#" .. "  " .. (#lsp_warns + #lsp_hints)
+  end
+  return sl
+end
+
 local function MyFileType()
   return string.format(" %s", vim.bo.filetype)
 end
@@ -96,7 +107,7 @@ local function MyEncode()
   if fileEncode == "" or fileEncode == nil then
     fileEncode = vim.go.encoding
   end
-  return string.format(" %s" , string.upper(fileEncode))
+  return string.format(" [%s] " , string.upper(fileEncode))
 end
 
 local function MyReadonly()
@@ -110,39 +121,80 @@ local function MyReadonly()
   end
 end
 
+local function GetFileIcon()
+  local filetype = vim.bo.filetype
+  local icon, color = require("nvim-web-devicons").get_icon_color_by_filetype(filetype)
+  if icon == nil or color == nil then
+    return ""
+  end
+  vim.cmd('highlight MyFileTypeColor guibg=#24273a guifg=' .. color)
+  local returnColor = "%#MyFileTypeColor#"
+  return returnColor .. icon .. " " .. filetype
+end
+
 Statusline = {}
-vim.cmd([[exec 'hi MODECOLOR gui=bold guifg=#eed49f guibg=' . synIDattr(synIDtrans(hlID('NvimTreeNormal')), 'bg', 'gui')]])
-vim.cmd('highlight MyStatusLineFlag guibg=#0096ff')
-vim.cmd('highlight MyStatusLineFlagI guibg=#F15412')
-vim.cmd('highlight MyStatusLineFlagV guibg=#FFFFDE')
-vim.cmd([[exec 'highlight MyFlagIcon gui=bold guifg=#0096ff guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
-vim.cmd([[exec 'highlight MyFlagIconI gui=bold guifg=#F15412 guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
-vim.cmd([[exec 'highlight MyFlagIconV gui=bold guifg=#FFFFDE guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
+
+vim.cmd('highlight MODECOLOR guifg=#e6e9ef guibg=#24273a')
+
+-- for new LSP error
+vim.cmd('highlight MyLspError guifg=#d20f39 guibg=#24273a')
+vim.cmd('highlight MyLspWarn guifg=#df8e1d guibg=#24273a')
+vim.cmd('highlight MyLspHint guifg=#179299 guibg=#24273a')
+
+-- for front part
+vim.cmd('highlight MyStatusLineFlag guibg=#8839ef guifg=#e6e9ef')
+vim.cmd('highlight MyStatusLineFlagI guibg=#d20f39 guifg=#e6e9ef')
+vim.cmd('highlight MyStatusLineFlagV guibg=#fe640b guifg=#e6e9ef')
+
+-- for icon, not use for this version 
+vim.cmd([[exec 'highlight MyFlagIcon gui=bold guifg=#8839ef guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
+vim.cmd([[exec 'highlight MyFlagIconI gui=bold guifg=#d20f39 guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
+vim.cmd([[exec 'highlight MyFlagIconV  gui=bold guifg=#fe640b guibg=' . synIDattr(synIDtrans(hlID('MODECOLOR')), 'bg', 'gui')]])
+
+-- for git part
+vim.cmd('highlight GitPart guifg=#000000 guibg=#cad3f5')
+vim.cmd('highlight MyGitIcon guifg=#fe640b guibg=#cad3f5')
+vim.cmd('highlight MyGitAdd guifg=#a6da95 guibg=#24273a')
+vim.cmd('highlight MyGitRemoved guifg=#ed8796 guibg=#24273a')
+vim.cmd('highlight MyGitChanged guifg=#eed49f guibg=#24273a')
+
+local function MyGitContent()
+  local mes = vim.b.gitsigns_status_dict
+  local status_mes = ''
+  if mes == nil then
+    return ""
+  else
+    status_mes = status_mes .. "%#MyGitIcon#" .. "  " .. "%#GitPart#" .. mes.head .. " "
+    if mes.added ~= nil then
+      status_mes = status_mes .. "%#MyGitAdd#" .. "  " .. mes.added
+    end
+    if mes.changed ~= nil then
+      status_mes = status_mes .. "%#MyGitChanged#" .. "  " .. mes.changed
+    end
+    if mes.removed ~= nil then
+      status_mes = status_mes .. "%#MyGitRemoved#" .. "  " .. mes.removed
+    end
+    return status_mes
+  end
+end
 
 Statusline.active = function ()
   return table.concat{
     MyUpdateColor(),
-    " ",
-    MyIconColor(),
-    " ",
-    Mymode(),
+    "  ",
+    MyGitContent(),
     "%#MODECOLOR#",
+    " ",
     "%m",
-    " %{get(b:,'gitsigns_status','')}",
-    "  ",
+    MyReadonly(),
     "%=",
-    "%<",
-    "%f",
-    "%=",
-    -- LspErrors(),
-    -- LspWarns(),
-    -- LspHints(),
-    " %{ObsessionStatus()}",
-    " Space: %{&tabstop}",
+    MyLspInfo(),
+    "%#MODECOLOR#",
     MyEncode(),
-    MyFileType(),
-    MyLineInfo(),
+    GetFileIcon(),
+    " ",
     MyUpdateColor(),
+    MyLineInfo(),
     " ",
   }
 end
