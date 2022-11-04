c6aa97cf0e8530c63184d23673946479fb0d2907
ccg <aimerchenyuan@163.com>
2022-11-04 19:38:12 +0800

    add color plugin config

--- nvim/lua/colorset.lua
+++ nvim/lua/colorset.lua
@@ -1,9 +1,20 @@
-vim.cmd('colorscheme catppuccin')
-
--- for some highlight group change
+-- for colorscheme setting
 vim.cmd([[
-" hi Normal guibg=None
-" exec 'hi BufferLineFill guibg=' . synIDattr(synIDtrans(hlID('Normal')), 'bg', 'gui')
-" exec 'hi BufferLineTabClose guibg=' . synIDattr(synIDtrans(hlID('Normal')), 'bg', 'gui')
-" hi FocusedSymbol gui=bold guifg=#fabd2f
+let g:gruvbox_material_enable_italic = 1
+let g:gruvbox_material_transparent_background = 0
+let g:gruvbox_material_visual = 'reverse'
 ]])
+
+
+vim.cmd('colorscheme gruvbox-material')
+
+local Color, colors, Group, groups, styles = require('colorbuddy').setup()
+
+-- Color.new("black", "#000000")
+-- -- name fg bg style guisp blend
+-- Group.new("CursorLine", colors.black, colors.orange, styles.NONE, colors.orange)
+-- Group.new("CursorLineNr", colors.yellow, colors.none, styles.NONE, colors.none)
+Color.new("my_base1", '#1d2021')
+Color.new("my_base2", '#3c3836')
+Group.new("CursorLine", colors.none, colors.my_base1, styles.NONE)
+Group.new("CursorLineNr", colors.orange, colors.none, styles.NONE)
